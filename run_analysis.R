41f5e23756fed03b62c5c38b0a64768600a3830b
Shane Mooney <smmoonshoes@gmail.com>
2016-04-24 16:25:59 -0700

    Annotating with activity labels

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 
 readAndMergeData <- function() {
-    trainingData <- read.csv2('train/X_train.txt', sep="", header=FALSE, skip=7000)
+    trainingData <- read.csv2('train/X_train.txt', sep="", header=FALSE)
     testData <- read.csv2('test/X_test.txt', sep="", header=FALSE)
     data <- rbind(trainingData, testData)
     data
@@ -15,12 +15,25 @@ labelData <- function(data) {
 }
 
 subsetMeanAndStdColumns <- function(data) {
-    data[, grep("mean|std", colnames(data))]
+    data[, grep("mean\\(\\)|std\\(\\)", colnames(data))]
+}
+
+extendActivityLabel <- function(data) {
+    activityLabels <- read.csv2('activity_labels.txt', sep="", header=FALSE)
+    trainingActivities <- read.csv2('train/y_train.txt', sep="", header=FALSE)
+    testActivities <- read.csv2('test/y_test.txt', sep="", header=FALSE)
+    
+    activities <- rbind(trainingActivities, testActivities)
+    labeledActivites <- merge(activities, activityLabels, all.x=T, sort=F, by.x="V1", by.y="V1")
+    data <- cbind(activity = as.character(labeledActivites[, "V2"]), data)
+    
+    data
 }
 
 runAnalysis <- function() {
     data <- readAndMergeData()
     data <- labelData(data)
-    meanAndStdData <- subsetMeanAndStdColumns(data)
-    meanAndStdData
+    data <- subsetMeanAndStdColumns(data)
+    data <- extendActivityLabel(data)
+    data
 }
