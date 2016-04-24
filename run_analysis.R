40787478bd302a8ca3e923d43aca1400bdc720f9
Shane Mooney <smmoonshoes@gmail.com>
2016-04-24 15:43:26 -0700

    Labelling data

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,19 @@
 
 readAndMergeData <- function() {
-    trainingData <- read.csv2('train/X_train.txt', sep="", header=FALSE)
-    testData <- read.csv2('test/X_test.txt', sep="", header=FALSE)
+    trainingData <- read.csv2('train/X_train.txt', sep="", header=FALSE, skip=7000)
+    testData <- read.csv2('test/X_test.txt', sep="", header=FALSE, skip=7000)
     data <- rbind(trainingData, testData)
     data
 }
 
+labelData <- function(data) {
+    labels <- read.csv2('features.txt', header=FALSE)
+    characterLabels <- as.character(labels[, "V1"])
+    strippedCharacterLabels <- unlist(lapply(strsplit(characterLabels, " "), function(x) x[2]))
+    names(data) <- strippedCharacterLabels
+}
+
 runAnalysis <- function() {
     data <- readAndMergeData()
+    labelData(data)
 }
